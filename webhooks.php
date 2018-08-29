1dcc117c48ee1ed23dfb4e646452a8c774d32f9d
Narttawat <fae3442@hotmail.com>
2018-08-29 15:34:31 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = 'P5YW5j+Ng0MaDhlhnKJmVOWb0LKEEzMC6BdEmvzaGhEtg+fJHbGHrMUu8dvk7kMvvB0T9m9zUDE/5f23360FXSp0DZSpJZb5CIeisjn+IPwF8C3f2qoHlEoWN28l4sw/sT1NqiuTlXT+XM5BkzP/zwdB04t89/1O/w1cDnyilFU=';
+$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
